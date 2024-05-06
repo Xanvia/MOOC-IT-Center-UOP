7d35d6c70200a2fa0fdecabdde37ca36f9650e72
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-06 17:50:27 +0530

    feat..

---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,20 @@
+// createa a basic login page ( page.tsx)
+
+import React from "react";
+import Link from "next/link";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      <Link href="/register">Register</Link>
+    </div>
+  );
+};
+
+export default Register;
