abe4b4d808ac7a07ecd639ae2c1d43278254d35d
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-06 13:49:50 +0530

    feat: first compononent

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      
+      <Navbar />
+      <h1>Welcome to Create Next App!</h1>
     </div>
   );
 }
