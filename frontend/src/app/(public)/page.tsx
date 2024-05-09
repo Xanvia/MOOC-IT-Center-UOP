8949f3446454e4f922b716508a39ddf18626b115
Lakshitha Eranga <90528675+sanjueranga@users.noreply.github.com>
2024-05-10 01:11:31 +0530

    Navbar (#26)
    * feat: tailwind navbar
    
    * feat: new profile button component
    
    * feat: navbar

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to Create Next App!</h1>
     </div>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#2563EB",
+        primary_test: "#072569",
+        button_yellow: "#FCD980",
+        secondary: "#2563EB",
+        text: "#333",
+        background: "#fff",
+      },
     },
   },
   plugins: [],
