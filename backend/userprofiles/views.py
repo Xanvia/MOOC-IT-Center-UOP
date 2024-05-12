f50e17cd513762cb5115fb04aca206682abfdd42
KulinduThenuka <kulinduthenuka@gmail.com>
2024-05-12 17:11:00 +0530

    feat:user

--- backend/userprofiles/views.py
+++ backend/userprofiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import UserProfile
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET','POST'])
+
+def user_list(request):
+    if request.method == 'GET':
+        drinks =   UserProfile.objects.all()
+        serializer= UserSerializer(drinks, many = True)
+        return JsonResponse({'users':serializer.data}, safe=False)
