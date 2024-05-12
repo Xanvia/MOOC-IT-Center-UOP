a6f49f4832ddd4c9a410f40c70d4388d422c31d2
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-12 21:46:54 +0530

    feat: conditon: is loggedin

--- frontend/src/components/Buttons/PrimaryButton.tsx
+++ frontend/src/components/Buttons/PrimaryButton.tsx
@@ -9,7 +9,7 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({ text }) => {
   return (
     <button
       type="button"
-      className="focus:outline-none text-black bg-white hover:bg-primary hover:ring-1 hover:text-white hover:ring-white focus:ring-4 focus:ring-primary_test font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:focus:ring-white"
+      className="focus:outline-none text-black font-times bg-white hover:bg-primary hover:ring-1 hover:text-white hover:ring-white focus:ring-4 focus:ring-primary_test font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:focus:ring-white"
     >
       {text}
     </button>
