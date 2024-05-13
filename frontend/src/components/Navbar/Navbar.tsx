5c8390bade2d5b0236fd12efe66339dddbe87128
Lakshitha Eranga <90528675+sanjueranga@users.noreply.github.com>
2024-05-13 17:29:28 +0530

    Navbar responsive (#28)
    * feat: new button
    
    * feat: hover
    
    * WIP
    
    * send profile menu away
    
    * WIP
    
    * feat: conditon: is loggedin

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,25 +3,31 @@ import React, { useState, useEffect, useRef } from "react";
 import ProfileButton from "./ProfileButton";
 import Link from "next/link";
 import PrimaryButton from "../Buttons/PrimaryButton";
+import SecondaryButton from "../Buttons/SecondaryButton";
 
 const Navbar = () => {
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const profileMenuRef = useRef(null);
+  const isLoggedIn = false;
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   const toggleProfileMenu = () => {
     setIsProfileMenuOpen(!isProfileMenuOpen);
   };
 
-  useEffect(() => {
-    const handleDocumentClick = (event: MouseEvent) => {
-      if (
-        profileMenuRef.current &&
-        (profileMenuRef.current as HTMLElement).contains(event.target as Node)
-      ) {
-        setIsProfileMenuOpen(false);
-      }
-    };
+  const handleDocumentClick = (event: MouseEvent) => {
+    if (
+      profileMenuRef.current &&
+      !(profileMenuRef.current as HTMLElement).contains(event.target as Node)
+    ) {
+      setIsProfileMenuOpen(false);
+    }
+  };
 
+  useEffect(() => {
     document.addEventListener("mousedown", handleDocumentClick);
     return () => {
       document.removeEventListener("mousedown", handleDocumentClick);
@@ -29,15 +35,16 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-primary_test">
+    <nav className="bg-primary">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             <button
               type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={toggleMobileMenu}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
@@ -73,11 +80,11 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <h1 className="text-white text-2xl font-bold">MOOC</h1>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch md:justify-start">
+            <div className="hidden sm:flex flex-shrink-0 items-center">
+              <h1 className="text-white text-3xl sm:font-bold">MOOC</h1>
             </div>
-            <div className="hidden py-10 pl-20 sm:ml-6 sm:block">
+            <div className="hidden py-5 md:py-10 lg:pl-20 md:ml-6 md:block lg:text-base text-sm">
               <div className="flex space-x-4">
                 <Link href="/" className="text-white hover:text-gray-300">
                   HOME
@@ -94,15 +101,49 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="flex items-center gap-10 mt-2 pr-20 justify-center">
-            <PrimaryButton text="Login" />
-            <PrimaryButton text="Register" />
+          <div className="flex items-center gap-5 mt-2 lg:pr-20 justify-center">
+            {isLoggedIn ? (
+              <div className="flex md:hidden absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="relative rounded-full bg-primary p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary_test"
+                >
+                  <span className="absolute -inset-1.5"></span>
+                  <span className="sr-only">View notifications</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
+                </button>
+
+                <ProfileButton
+                  isProfileMenuOpen={isProfileMenuOpen}
+                  toggleProfileMenu={toggleProfileMenu}
+                  profileMenuRef={profileMenuRef}
+                />
+              </div>
+            ) : (
+              <>
+                <SecondaryButton text="Login" />
+                <PrimaryButton text="Register" />
+              </>
+            )}
           </div>
 
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="hidden absolute inset-y-0 right-0 md:flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              className="relative rounded-full bg-primary p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary_test"
             >
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">View notifications</span>
@@ -130,36 +171,36 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="#"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Calendar
-          </a>
+      {isMobileMenuOpen && (
+        <div className="md:hidden" id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              HOME
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              ABOUT US
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              CONTACTS
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              FAQ
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
