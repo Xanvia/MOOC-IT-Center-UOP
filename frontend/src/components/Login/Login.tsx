24edd042f9bb1e2bafb9ad8dc4201503ac7c5208
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-15 22:22:25 +0530

    feat: click outside to close

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -6,6 +6,15 @@ import SecondaryButton from "../Buttons/SecondaryButton";
 export default function Login() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    
+  };
+
+  const handleInsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsOpen(false);
+  };
+
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
@@ -27,12 +36,37 @@ export default function Login() {
           id="authentication-modal"
           aria-hidden="true"
           className="fixed inset-0 flex items-center justify-center h-full w-full bg-black bg-opacity-50 pt-5"
+          onMouseDown={handleInsideClick}
         >
-          <div className="relative bg-white rounded-lg shadow-2xl w-[450px] h-[720px] px-16 pt-4">
-            
+          <div
+            onMouseDown={handleOutsideClick}
+            className="relative bg-white rounded-lg shadow-2xl w-[450px] h-[720px] px-16 pt-4"
+          >
+            <button
+              type="button"
+              className="absolute top-0 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+              data-modal-hide="authentication-modal"
+              onClick={toggleModal}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
             <div className="text-3xl font-bold text-[#072569] text-center mt-5 mb-2.5 mx-0">
               Welcome Back!
-              
             </div>
 
             <form action="#">
@@ -85,29 +119,6 @@ export default function Login() {
               </a>
             </div>
           </div>
-          <button
-                type="button"
-                className="absolute top-0 right-0end-2.5 text-gray-400  bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                data-modal-hide="authentication-modal"
-                onClick={toggleModal}
-              >
-                <svg
-                  className="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                  />
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
         </div>
       )}
     </>
