0ee27afc591fd63b709d6330deda83b344b0f0a0
Sandakelum Sameera Bandara <156235286+SandakelumSameera@users.noreply.github.com>
2024-05-15 23:21:28 +0530

    8 frontend login (#36)
    * feat: layout
    
    * feat: model on center
    
    * feat: close button
    
    * WIP
    
    * feat:frontend_login
    
    * WIP
    
    * feat: navbar buttons
    
    * WIP
    
    * feat: click outside to close
    
    * feat: login modal
    
    * feat: login final
    
    * feat: registration zoom in
    
    * feat: click out side to close
    
    * feat:set paddings
    
    ---------
    
    Co-authored-by: Lakshitha Eranga <sanjueranga16@gmail.com>
    Co-authored-by: AselDamiska <162301283+AselDamiska@users.noreply.github.com>

---
+++ frontend/src/components/Login/Login.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState } from "react";
+import SecondaryButton from "../Buttons/SecondaryButton";
+import SolidButton from "../Buttons/SolidButton";
+import SvgButton from "../Buttons/SvgButton";
+import GoogleIcon from "@/icons/GoogleIcon";
+
+export default function Login() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  const handleInsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsOpen(false);
+  };
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <>
+      <SecondaryButton onClick={toggleModal} text="Login" />
+
+      {isOpen && (
+        <div
+          id="authentication-modal"
+          aria-hidden="true"
+          className="fixed inset-0 flex items-center justify-center h-full w-full bg-black bg-opacity-50 pt-5"
+          onMouseDown={handleInsideClick}
+        >
+          <div
+            onMouseDown={handleOutsideClick}
+            className="relative h-full w-full bg-white rounded-lg shadow-2xl sm:w-[450px] sm:h-[720px] px-16 pt-4"
+          >
+            <button
+              type="button"
+              className="absolute top-0 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+              data-modal-hide="authentication-modal"
+              onClick={toggleModal}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="text-3xl font-bold text-[#072569] text-center mt-10 mb-2.5 mx-0">
+              Welcome Back!
+            </div>
+
+            <form action="#">
+              <div className="pt-16 grid grid-cols-1 gap-8">
+                <div className="w-80 md:w-auto mx-auto md:mx-0">
+                  <div className="relative w-full min-w-[200px] h-10">
+                    <input
+                      className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-primary focus:border-gray-900"
+                      placeholder=" "
+                    />
+                    <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-primary leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-500 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                      Email
+                    </label>
+                  </div>
+                </div>
+                <div className="w-80 md:w-auto mx-auto md:mx-0">
+                  <div className="relative w-full min-w-[200px] h-10">
+                    <input
+                      className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-primary focus:border-gray-900"
+                      placeholder=" "
+                    />
+                    <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-primary leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-500 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                      Password
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+            <div className="pt-16 sm:px-8 flex flex-col items-center justify-center">
+              <SolidButton text="R E G I S T E R" onClick={() => {}} />
+              <br />
+              <div className="text-gray-500 peer-focus:text-gray-500 py-2 px-28 text-center">
+                <p> -or- </p>
+              </div>
+              <SvgButton
+                text="Continue with google"
+                onClick={() => {}}
+                svg={<GoogleIcon />}
+              />
+              <br />
+              <div className="text-blue-950 pt-16 text-center">
+                Already have an account? <u>Login</u>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -9,6 +9,14 @@ import SvgButton from "../Buttons/SvgButton";
 export default function Register() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  const handleInsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsOpen(false);
+  };
+
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
@@ -28,28 +36,32 @@ export default function Register() {
         <div
           id="authentication-modal"
           aria-hidden="true"
-          className="fixed inset-0 flex items-center justify-center h-full w-full bg-black bg-opacity-50"
+          className="fixed z-10 inset-0 flex items-center justify-center h-full w-full bg-black bg-opacity-50"
+          onMouseDown={handleInsideClick}
         >
-          <div className="flex md:rounded-lg md:shadow-2xl md:w-4/6 md:h-4/5 h-full w-full">
+          <div
+            onMouseDown={handleOutsideClick}
+            className="flex md:rounded-lg md:shadow-2xl md:w-[1000px] xl:w-[1150px] md:h-[700px] h-full w-full"
+          >
             <div className="hidden md:flex relative basis-4/12">
               <div className="bg-primary text-white px-10 py-4 rounded-l-lg absolute inset-0 flex pt-60 px-10 ">
                 <div className="">
                   <h1 className="text-4xl lg:text-6xl font-bold mb-4">MOOC</h1>
-                  <span className="font-sans text-base xl:text-2xl ">
+                  <span className="font-sans text-base xl:text-xl ">
                     {`"Empower your journey. Learn, grow, succeed with us."`}
                   </span>
                 </div>
               </div>
             </div>
             <div className="relative w-full h-11/12 bg-white md:rounded-r-lg md:basis-8/12 pt-10 pl-12 pr-10">
-              <h1 className="ps-5 py-1 lg:py-4 text-3xl text-primary font-bold mb-4">
+              <h1 className="ps-5 py-1 lg:py-4 text-2xl text-primary font-bold mb-4">
                 Take the First Step!
               </h1>
               <Dropdown />
               <br />
               <center>
                 <form>
-                  <div className="md:px-5 lg:px-10 md:pt-10 grid grid-cols-1 lg:grid-cols-2  gap-6 md:gap-2 xl:gap-8">
+                  <div className="md:px-5 lg:px-10 md:pt-6 grid grid-cols-1 lg:grid-cols-2  gap-6 md:gap-2 xl:gap-8">
                     <div className="w-80 md:w-auto mx-auto md:mx-0">
                       <div className="relative w-full min-w-[200px] h-10">
                         <input
@@ -118,7 +130,7 @@ export default function Register() {
                     </div>
                   </div>
                   <br />
-                  <div className="lg:pt-8">
+                  <div className="lg:pt-6">
                     <SolidButton text="R E G I S T E R" onClick={() => {}} />
                     <br />
                     <div className="text-gray-500 peer-focus:text-gray-500 py-2">
