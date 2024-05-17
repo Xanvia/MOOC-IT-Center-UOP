8705d6c0f668f9017726baef894b6385cc2cc0c0
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-17 23:21:36 +0530

    feat: google register frontend

--- frontend/src/components/Buttons/SvgButton.tsx
+++ frontend/src/components/Buttons/SvgButton.tsx
@@ -11,6 +11,7 @@ const SvgButton: React.FC<ButtonProps> = ({ text, onClick, svg }) => {
     <button
       className="bg-primary px-5 min-w-64 hover:bg-primary_test align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-16 rounded-lg bg-accent text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
       type="button"
+      onClick={onClick}
     >
       <div className="flex pl-2">
         {svg}
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -29,6 +29,33 @@ export default function Register() {
     console.log("Password:", password);
   };
 
+  const openGoogleLoginPage = () => {
+    const GOOGLE_CLIENT_ID =
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "";
+    const GOOGLE_CLIENT_SECRET =
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "";
+    const googleAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    const scope = [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" ");
+
+    const params = new URLSearchParams({
+      response_type: "code",
+      client_id: GOOGLE_CLIENT_ID,
+      secret: GOOGLE_CLIENT_SECRET,
+      redirect_uri: "http://127.0.0.1:8000/auth/google/register/redirect",
+      prompt: "select_account",
+      access_type: "online",
+      scope,
+    });
+
+    const url = `${googleAuthUrl}?${params}`;
+
+    window.location.href = url;
+  };
+
   return (
     <>
       ,<PrimaryButton onClick={toggleModal} text="Register" />
@@ -138,7 +165,7 @@ export default function Register() {
                     </div>
                     <SvgButton
                       text="Continue with google"
-                      onClick={() => {}}
+                      onClick={openGoogleLoginPage}
                       svg={<GoogleIcon />}
                     />
                     <br />
