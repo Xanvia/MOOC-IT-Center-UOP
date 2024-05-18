f7206caf62dbdb7249bc9625428704938482bcad
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-18 11:17:48 +0530

    WIP

--- frontend/src/app/api/auth/google/callback/route.ts
+++ frontend/src/app/api/auth/google/callback/route.ts
@@ -6,8 +6,9 @@ export async function GET(request: NextRequest) {
   const code = requestUrl.searchParams.get("code");
 
   const HOST = process.env.NEXT_PUBLIC_HOST ?? "http://127.0.0.1:8000";
-
+  const user_type = "student";
   const response = await axios.post(`${HOST}/api/user/google-auth/`, {
+    user_type,
     code,
     redirect_uri: "http://localhost:3000/api/auth/google/callback",
   });
