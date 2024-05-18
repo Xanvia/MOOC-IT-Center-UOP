452323be173c95aa7b7676b6ccb22324e598f7b3
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-18 15:53:27 +0530

    WIP

--- frontend/src/components/Navbar/ProfileButton.tsx
+++ frontend/src/components/Navbar/ProfileButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { RefObject } from "react";
+const DefaultProfileImage = "/images/defaultuser.png";
 
 interface ProfileButtonProps {
   isProfileMenuOpen: boolean;
@@ -27,7 +28,7 @@ const ProfileButton = ({
           <span className="sr-only">Open user menu</span>
           <img
             className="h-8 w-8 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src={DefaultProfileImage}
             alt=""
           />
         </button>
