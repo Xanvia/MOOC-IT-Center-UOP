32fc6cef0532e678076cf0d20b714f8f6300e470
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-18 18:01:55 +0530

    refactor: logo names

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -5,7 +5,7 @@ import "@/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MOOC",
+  title: "OpenEd",
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} >{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
