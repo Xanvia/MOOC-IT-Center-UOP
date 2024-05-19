c4809a09938b071473983173a50ec94d9a88dfd8
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-19 16:01:26 +0530

    feat: google login backend

--- backend/userprofiles/urls.py
+++ backend/userprofiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationApiView, GoogleAuthRegisterView
+from .views import UserRegistrationApiView, GoogleAuthRegisterView,UserLoginApiView,GoogleLoginApiView
 
 urlpatterns = [
     path("register/", UserRegistrationApiView.as_view(), name="user-registration"),
+    path("google-auth/login/", GoogleLoginApiView.as_view(), name="user-login-google"),
     path("google-auth/", GoogleAuthRegisterView.as_view(), name="google-auth"),
+    path("login/", UserLoginApiView.as_view(), name="user-login"),
 ]
--- backend/userprofiles/views.py
+++ backend/userprofiles/views.py
@@ -45,3 +45,70 @@ class GoogleAuthRegisterView(generics.CreateAPIView):
 
         return Response(response.data, status=status.HTTP_200_OK)
     
+class UserLoginApiView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_name = serializer.validated_data["username"]
+        password = serializer.validated_data["password"]
+        user = authenticate(username=user_name, password=password)
+        try:
+            user_profile = UserProfile.objects.get(user=user) if user else None
+        except UserProfile.DoesNotExist:
+            user_profile = None
+        if user:
+            respObj = {
+                "status": "success",
+                "data": {
+                    "access_token": str(AccessToken.for_user(user)),
+                    "user": {
+                        "user_id": user.id,
+                        "username": user.username,
+                        "full_name": f"{user.first_name} {user.last_name}",
+                        "email": user.email,
+                        "profile_picture": user_profile.profile_picture if user_profile else None,
+                    },
+                },
+            }
+            return Response(respObj, status=status.HTTP_200_OK)
+        else:
+            respObj = {
+                "status": "fail",
+                "message": ["Invalid email or password"],
+            }
+
+        return Response(respObj, status=status.HTTP_403_FORBIDDEN)
+    
+
+class GoogleLoginApiView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        code = request.data.get("code")
+        redirect_uri = request.data.get("redirect_uri")
+        try:
+            user_data = google_authenticate(code, redirect_uri)
+        except Exception as e:
+            print(e)
+        try: 
+            user = User.objects.get(email=user_data.get("email"))
+        except User.DoesNotExist:
+            raise PermissionDenied("User does not exist")
+        user_profile = user.userprofile
+        respObj = {
+            "status": "success",
+            "data": {
+                "access_token": str(AccessToken.for_user(user)),
+                "user": {
+                    "user_id": user.id,
+                    "username": user.username,
+                    "full_name": f"{user.first_name} {user.last_name}",
+                    "email": user.email,
+                    "profile_picture": user_profile.profile_picture if user_profile else None,
+                },
+            },
+        }
+        return Response(respObj, status=status.HTTP_200_OK)
