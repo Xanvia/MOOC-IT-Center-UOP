5400a662f40174ef8c87393d1c8b016e3ac802c5
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-20 15:04:09 +0530

    fix: add image

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Navbar from "@/components/Navbar/Navbar";
 import Search from "@/components/Search/Search";
-import CourseCard from "@/components/Course/CourseCard";
+import CourseCard from "@/components/Course/CourseCard/CourseCard";
+
+const course1 = "/images/course1.png";
+const course2 = "/images/course2.png";
+const course3 = "/images/course3.png";
+
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get("token"));
 
@@ -24,19 +29,30 @@ export default function Home() {
     <div>
       <Navbar isLoggedIn={isLoggedIn} onSignOut={handleSignOut} />
       <Search />
-    <div className="flex justify-center space-x-1">
+      <div className="flex justify-center space-x-1">
         <div>
           <image></image>
-          <CourseCard title="Course 1" description="Description 1" />
+          <CourseCard
+            title="Computer Networks "
+            description="The learning material on beginner-level website creation."
+            image={course1}
+          />
         </div>
         <div>
-          <CourseCard title="Course 2" description="Description 2" />
+          <CourseCard
+            title="Digital Marketing"
+            description="Material on beginner marketing strategies and concepts"
+            image={course2}
+          />
         </div>
         <div>
-          <CourseCard title="Course 3" description="Description 3" />
+          <CourseCard
+            title="Data Analysis"
+            description="Description 3"
+            image={course3}
+          />
         </div>
-    </div>
-      
+      </div>
     </div>
   );
 }
