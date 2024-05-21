67d67d63213de1bb753386f534f1679e5b62acde
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-21 19:10:33 +0530

    feat: toast components

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,3 +2,8 @@ export const HOST = process.env.NEXT_PUBLIC_HOST;
 export const CALLBACK_URL = process.env.NEXT_PUBLIC_CALLBACK_URL;
 export const API_URL = `${HOST}/api`;
 export const redirect_uri = "http://localhost:3000/api/auth/google/callback";
+
+export interface ToastProps {
+  message: string;
+  onClose: () => void;
+}
