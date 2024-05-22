dbb887afa54fcf052d4f80b957146f80cc877ad0
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-22 19:48:57 +0530

    feat: home page cards responsive

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { Toaster } from "sonner";
+import Navbar from "@/components/Navbar/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toaster richColors closeButton position="top-right" />
+        <Navbar />
         {children}
       </body>
     </html>
--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,42 +1,77 @@
 "use client";
 import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import Navbar from "@/components/Navbar/Navbar";
 import Search from "@/components/Search/Search";
 import CourseCard from "@/components/Course/CourseCard/CourseCard";
 
 const course1 = "/images/course1.png";
 const course2 = "/images/course2.png";
 const course3 = "/images/course3.png";
+const course6 = "/images/course6.png";
+const course5 = "/images/course5.png";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <>
       <Search />
-      <div className="flex justify-center space-x-1">
-        <div>
-          <CourseCard
-            title="Computer Networks "
-            description="The learning material on beginner-level website creation."
-            image={course1}
-          />
-        </div>
-        <div>
-          <CourseCard
-            title="Digital Marketing"
-            description="Material on beginner marketing strategies and concepts"
-            image={course2}
-          />
-        </div>
-        <div>
-          <CourseCard
-            title="Data Analysis"
-            description="Description 3"
-            image={course3}
-          />
-        </div>
+      <div className="flex grid grid-cols-1 py-10 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 justify-center items-center mx-10 sm:mx-36 lg:mx-36 gap-4  lg:gap-4 2xl:gap-10">
+        <CourseCard
+          title="Introduction to Computer Networks "
+          description="The learning material on beginner-level website creation."
+          image={course1}
+        />
+
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course2}
+        />
+
+        <CourseCard
+          title="Data Analysis"
+          description="Description 3"
+          image={course3}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course5}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course2}
+        />
+        <CourseCard
+          title="Computer Networks "
+          description="The learning material on beginner-level website creation."
+          image={course1}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course5}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course2}
+        />
+        <CourseCard
+          title="Computer Networks "
+          description="The learning material on beginner-level website creation."
+          image={course1}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course6}
+        />
+        <CourseCard
+          title="Digital Marketing"
+          description="Material on beginner marketing strategies and concepts"
+          image={course2}
+        />
       </div>
-    </div>
+    </>
   );
 }
