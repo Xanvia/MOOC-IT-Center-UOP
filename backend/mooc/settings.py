6934a00083b644927f95aee87b7023b0b057fb51
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-24 11:58:44 +0530

    feat: added permissons migrations and tests

--- backend/mooc/settings.py
+++ backend/mooc/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
     ],
     "EXCEPTION_HANDLER": "mooc.utils.custom_exception_handler",
 }
