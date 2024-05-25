1557235a0e665e1b2faa0341881d532bd9d2fac4
KulinduThenuka <kulinduthenuka@gmail.com>
2024-05-25 09:37:33 +0530

    feat: Get all countries in DB

--- backend/userprofiles/views.py
+++ backend/userprofiles/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import (
     UserSerializer,
@@ -162,7 +163,7 @@ class AddUserInfoView(generics.UpdateAPIView):
 
 
 class CountryListAPIView(generics.ListAPIView):
-    queryset = Country.objects.all()
+    queryset = Country.objects.all().order_by('label')
     serializer_class = CountrySerializer
     pagination_class = None
 
