26a893dc1bad92e4f2488c506b1551f42f328e02
KulinduThenuka <kulinduthenuka@gmail.com>
2024-05-26 21:52:50 +0530

    feat:profile page initial

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -6,10 +6,23 @@ export default function page() {
   return (
     <>
         <Navbar/>
-        <div className="grid grid-cols-3 h-full ">
-            <div className="bg-red-400">
+        <div className="grid grid-cols-3 h-full  ">
+            <div className="bg-sky-100">
                 <div className="grid grid-rows-3 grid-flow-col h-full">
-                    <div className="row-start-1 row-end-4 bg-white m-10 ">03</div>
+                    <div className="row-start-1 row-end-4 bg-white m-10 ">
+                        <div className="relative h-32 w-32 ...">
+                            <div className="absolute bg-black inset-x-10 top-10 h-16 rounded-full ">
+                                <img 
+                                    src="/profile.png"
+                                    width={500}
+                                    height={500}
+                                    alt="Picture of the author"
+                                />
+                                02
+                            </div>
+                        </div>
+                 
+                    </div>
                 </div>
                 
                 05
@@ -21,7 +34,7 @@ export default function page() {
             <div className="col-span-2 bg-sky-100">
                 <div className="flex flex-col h-full">
 
-                    <div className="bg-sky-200 h-full basis-3/5">
+                    <div className="bg-sky-100 h-full basis-3/5">
                         
                         <div className="flex flex-col h-full">
 
@@ -37,7 +50,7 @@ export default function page() {
                         </div>   
                     </div>
 
-                    <div className="bg-sky-400 h-full basis-2/5">
+                    <div className="bg-sky-100 h-full basis-2/5">
 
                         <h1 className='font-bold text-2xl mx-10'>Education</h1>
 
