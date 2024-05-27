7aff034ba35cfe7801a1a6287ff894ff82aae5fd
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-27 19:54:43 +0530

    feat: profile section done

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,12 +1,15 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import ProfileInfo from "./ProfileInfo";
+import EditButton from "../Buttons/EditButton";
 const DefaultProfileImage = "/images/defaultuser.png";
 
 export default function Profile() {
   return (
     <div className="basis-1/2">
-      <div className="bg-white min-h-full w-7/12 ml-48 rounded-lg shadow-md py-14 flex flex-col items-center justify-center">
+      <div className="relative bg-white flex min-h-full w-7/12 ml-48 rounded-lg shadow-md py-14 flex flex-col items-center justify-center">
+        <EditButton onClick={() => {}} />
         <Image
           src={DefaultProfileImage}
           alt="Profile Image"
