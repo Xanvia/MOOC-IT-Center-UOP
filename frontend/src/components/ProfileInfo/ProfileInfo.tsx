b27dad66477ded80ad9efe5a900865eeda6272c4
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-27 13:14:04 +0530

    feat: fill profile details

--- frontend/src/components/ProfileInfo/ProfileInfo.tsx
+++ frontend/src/components/ProfileInfo/ProfileInfo.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const ProfileInfo: React.FC = ({}) => {
   return (
-    <div className="Box bg-sky-100 relative border-2 py-8 mt-2 h-full w-full">
-        <div className="grid grid-cols-2 gap-4 text-primary">
-            <div><h1>01</h1></div>
-            <div><h1>02</h1></div>
-            <div><h1>03</h1></div>
-            <div><h1>04</h1></div>
-            <div><h1>05</h1></div>
-            <div><h1>06</h1></div>
-            <div><h1>07</h1></div>
-            <div><h1>08</h1></div>
+    <div className="Box bg-white relative py-8 mt-2 h-full w-full">
+        <div className="grid grid-cols-2 gap-4 text-primary text-right mr-24 ml-24">
+            <div><div className="bg-slate-500 h-4 w-4 rounded-full "></div></div>
+            <div><h1>johndoes@gmail.com</h1></div>
+            <div><div className="bg-slate-500 h-4 w-4 rounded-full "></div></div>
+            <div><p>+94-765432121</p></div>
+            <div><div className="bg-slate-500 h-4 w-4 rounded-full "></div></div>
+            <div><p>2000 - 10 - 12</p></div>
+            <div><div className="bg-slate-500 h-4 w-4 rounded-full "></div></div>
+            <div><h1>Sri Lanka</h1></div>
         </div>
-        <div className="text text-center text-primary mt-5 ml-5 mr-5">
+        <div className="text text-center text-primary mt-8 ml-10 mr-10">
             <h1>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
         </div>
     </div>
