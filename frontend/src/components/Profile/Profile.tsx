ec37d1800801a527b92cf26b1cc7edd4cf78260a
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-28 22:16:51 +0530

    feat: fix profile comp

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -5,7 +5,18 @@ import ProfileInfo from "./ProfileInfo";
 import EditProfileModal from "./EditProfile";
 const DefaultProfileImage = "/images/52.jpg";
 
-export default function Profile() {
+interface ProfileProps {
+  data: {
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string;
+    birthdate: string;
+    location: string;
+  };
+}
+
+const Profile: React.FC<ProfileProps> = ({ data }) => {
   return (
     <div className="basis-1/2">
       <div className="relative bg-white min-h-full w-full sm:w-7/12 sm:ml-40 lg:ml-48 rounded-lg shadow-md py-14 flex flex-col items-center justify-center">
@@ -17,9 +28,19 @@ export default function Profile() {
           height={120}
           className="rounded-full ring-4 ring-primary_light"
         />
-        <h1 className="text text-primary text-3xl pt-3">John Doe</h1>
-        <ProfileInfo />
+        <h1 className="text text-primary text-3xl pt-3">
+          {data.firstname} {data.lastname}
+        </h1>
+        <ProfileInfo 
+          email={data.email}
+          phone={data.phone}
+          birthdate={data.birthdate}
+          location={data.location}
+        />
       </div>
     </div>
   );
-}
+};
+
+export default Profile;
+
