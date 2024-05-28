962db8431b0ecb487bc32de20466bba2c50455bf
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-28 22:14:27 +0530

    feat: interface profileinfo

--- frontend/src/components/Profile/ProfileInfo.tsx
+++ frontend/src/components/Profile/ProfileInfo.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-const ProfileInfo: React.FC = ({}) => {
+interface ProfileInfoProps {
+  email: string;
+  phone: string;
+  birthdate: string;
+  location: string;
+}
+
+const ProfileInfo: React.FC<ProfileInfoProps> = ({ email, phone, birthdate, location }) => {
   return (
     <div className="Box bg-white mt-6 h-full w-full ">
       <div className="flex py-10">
         <ul className="space-y-6 text-right text-gray-600 mx-auto">
           <li className="flex items-center justify-between space-x-20 rtl:space-x-reverse">
             <svg
-              className="w-6 h-6 text-primary "
+              className="w-6 h-6 text-primary"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -18,9 +25,9 @@ const ProfileInfo: React.FC = ({}) => {
               <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z" />
               <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z" />
             </svg>
-            <span>sanjueranga16@gmail.com</span>
+            <span>{email}</span>
           </li>
-          <li className="flex items-center justify-between space-x-20  rtl:space-x-reverse">
+          <li className="flex items-center justify-between space-x-20 rtl:space-x-reverse">
             <svg
               className="w-6 h-6 text-primary"
               aria-hidden="true"
@@ -38,8 +45,7 @@ const ProfileInfo: React.FC = ({}) => {
                 d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"
               />
             </svg>
-
-            <span>+94 74 5632 123</span>
+            <span>{phone}</span>
           </li>
           <li className="flex items-center justify-between space-x-20 rtl:space-x-reverse">
             <svg
@@ -53,8 +59,7 @@ const ProfileInfo: React.FC = ({}) => {
             >
               <path d="M20 7h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C10.4 2.842 8.949 2 7.5 2A3.5 3.5 0 0 0 4 5.5c.003.52.123 1.033.351 1.5H4a2 2 0 0 0-2 2v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V9a2 2 0 0 0-2-2Zm-9.942 0H7.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM13 14h-2v8h2v-8Zm-4 0H4v6a2 2 0 0 0 2 2h3v-8Zm6 0v8h3a2 2 0 0 0 2-2v-6h-5Z" />
             </svg>
-
-            <span>12 - 10 - 2000 </span>
+            <span>{birthdate}</span>
           </li>
           <li className="flex items-center justify-between space-x-20 rtl:space-x-reverse">
             <svg
@@ -81,8 +86,7 @@ const ProfileInfo: React.FC = ({}) => {
                 d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"
               />
             </svg>
-
-            <span>Sri Lanka</span>
+            <span>{location}</span>
           </li>
         </ul>
       </div>
