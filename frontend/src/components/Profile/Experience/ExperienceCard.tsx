faa52773569f19c8d08cf93789a8687421c8ee01
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-29 21:34:19 +0530

    WIP

--- frontend/src/components/Profile/Experience/ExperienceCard.tsx
+++ frontend/src/components/Profile/Experience/ExperienceCard.tsx
@@ -1,23 +1,26 @@
 "use client";
 import React from "react";
 import AddExperienceModal from "./ExperienceModal";
+import { Work } from "../types";
 
-const ExperienceCard: React.FC = ({}) => {
+interface props {
+  workData: Work;
+}
+
+const ExperienceCard: React.FC<props> = ({ workData }) => {
   return (
     <div className="relative flex Box bg-white md:rounded-lg shadow-md border-2 px-4 py-4 h-40 sm:w-5/6 w-full my-5 mb-8">
-      <AddExperienceModal CardTitle="Edit Work Experience" Action="Edit"/>
+      <AddExperienceModal CardTitle="Edit Work Experience" Action="Edit" />
       <div className="flex flex-col ml-4 w-full">
         <div className="text text-xl font-semibold border-b border-gray-300 pb-2 pt-1">
-          <h1> University of Peradeniya</h1>
+          <h1>{workData.company}</h1>
         </div>
         <div className="text text-gray-600 mt-4">
-          <h1> Lecturer | Faculty Of Science </h1>
+          <h1>{workData.position}</h1>
         </div>
         <div className="text mt-2 text-right font-medium mr-2">
           <h1>
-            {" "}
-            October <span className="text-primary">2020</span> - December{" "}
-            <span className="text-primary">2021</span>
+            {workData.startDate} - {workData.endDate}
           </h1>
         </div>
       </div>
--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,41 +1,79 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import ProfileInfo from "./ProfileInfo";
 import EditProfileModal from "./EditProfile";
-import { ProfileData } from "@/components/Profile/types";
+import { ProfileData, Work, Education } from "@/components/Profile/types";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { API_URL } from "@/utils/constants";
 const DefaultProfileImage = "/images/52.jpg";
+import { toast } from "sonner";
+
+const token = Cookies.get("token");
 interface ProfileProps {
-  userData: ProfileData;
-  setProfileData: React.Dispatch<React.SetStateAction<ProfileData | undefined>>;
+  setWork: React.Dispatch<React.SetStateAction<Work[]>>;
+  setEducation: React.Dispatch<React.SetStateAction<Education[]>>;
 }
 
-const Profile: React.FC<ProfileProps> = ({ userData, setProfileData }) => {
+const Profile: React.FC<ProfileProps> = ({ setEducation, setWork }) => {
+  const [profileData, setProfileData] = useState<ProfileData | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/user/profile/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setProfileData(response.data.data);
+        setWork(response.data.data.work_experiences);
+        setEducation(response.data.data.educations);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfileData();
+  }, []);
+
   return (
     <div className="basis-1/2">
       <div className="relative bg-white min-h-full w-full sm:w-7/12 sm:ml-40 lg:ml-48 rounded-lg shadow-md py-14 flex flex-col items-center justify-center">
-        <EditProfileModal userData={userData} setProfileData={setProfileData} />
-        <Image
-          src={
-            userData.profile_image ||
-            userData.profile_picture ||
-            DefaultProfileImage
-          }
-          alt="Profile Image"
-          width={120}
-          height={120}
-          className="rounded-full ring-4 ring-primary_light"
-        />
-        <h1 className="text text-primary text-3xl pt-3">
-          {userData.firstname} {userData.lastname}
-        </h1>
-        <ProfileInfo
-          email={userData.email}
-          mobile_nubmer={userData.mobile_number}
-          birth_date={userData.birth_date}
-          location={userData.country.label}
-          description={userData.description}
-        />
+        {profileData ? (
+          <>
+            <EditProfileModal
+              userData={profileData}
+              setProfileData={setProfileData}
+            />
+            <Image
+              src={
+                profileData.profile_image ||
+                profileData.profile_picture ||
+                DefaultProfileImage
+              }
+              alt="Profile Image"
+              width={120}
+              height={120}
+              className="rounded-full ring-4 ring-primary_light"
+            />
+            <h1 className="text text-primary text-3xl pt-3">
+              {profileData.firstname} {profileData.lastname}
+            </h1>
+            <ProfileInfo
+              email={profileData.email}
+              mobile_nubmer={profileData.mobile_number}
+              birth_date={profileData.birth_date}
+              location={profileData.country.label}
+              description={profileData.description}
+            />
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );
