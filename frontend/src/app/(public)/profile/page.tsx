3e9b2e06936ab8915521edef5b3c7836f0556374
AselDamsika <162301283+AselDamiska@users.noreply.github.com>
2024-05-29 11:53:13 +0530

    72 integraton get userprofile page (#77)
    * feat: initial setup
    
    * feat: interface profileinfo
    
    * feat: fix profile comp
    
    * feat: page with axios
    
    * feat: userprofile types
    
    * feat: map profile data
    
    * WIP
    
    ---------
    
    Co-authored-by: Lakshitha Eranga <sanjueranga16@gmail.com>

--- frontend/src/app/(public)/profile/page.tsx
+++ frontend/src/app/(public)/profile/page.tsx
@@ -1,16 +1,43 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Profile from "@/components/Profile/Profile";
 import ExperienceModal from "@/components/Profile/Experience/ExperienceModal";
 import ExperienceCard from "@/components/Profile/Experience/ExperienceCard";
 import EducationCard from "@/components/Profile/Education/EducatonCard";
 import EducationModal from "@/components/Profile/Education/EducationModal";
+import { Work, Education, ProfileData } from "@/components/Profile/types";
+import Cookies from "js-cookie";
+import { API_URL } from "@/utils/constants";
 
-export default function page() {
+const token = Cookies.get("token");
+
+export default function ProfilePage() {
+  const [profileData, setProfileData] = useState<ProfileData | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/user/profile/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setProfileData(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfileData();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col lg:flex-row w-full py-20">
-        <Profile />
+        {profileData && <Profile userData={profileData} />}
         <div className="relative  lg:w-full h-11/12  md:rounded-r-lg lg:basis-1/2 2xl:px-12 basis-1/3 mx-6 sm:ml-32 pt-32 lg:pt-0 lg:m-0">
           <div className="">
             <ExperienceModal
--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -3,23 +3,37 @@ import React from "react";
 import Image from "next/image";
 import ProfileInfo from "./ProfileInfo";
 import EditProfileModal from "./EditProfile";
+import { ProfileData } from "@/components/Profile/types";
 const DefaultProfileImage = "/images/52.jpg";
+interface ProfileProps {
+  userData: ProfileData;
+}
 
-export default function Profile() {
+const Profile: React.FC<ProfileProps> = ({ userData }) => {
   return (
     <div className="basis-1/2">
       <div className="relative bg-white min-h-full w-full sm:w-7/12 sm:ml-40 lg:ml-48 rounded-lg shadow-md py-14 flex flex-col items-center justify-center">
         <EditProfileModal />
         <Image
-          src={DefaultProfileImage}
+          src={userData.profile_picture || DefaultProfileImage}
           alt="Profile Image"
           width={120}
           height={120}
           className="rounded-full ring-4 ring-primary_light"
         />
-        <h1 className="text text-primary text-3xl pt-3">John Doe</h1>
-        <ProfileInfo />
+        <h1 className="text text-primary text-3xl pt-3">
+          {userData.full_name}
+        </h1>
+        <ProfileInfo
+          email={userData.email}
+          mobile_nubmer={userData.mobile_number}
+          birth_date={userData.birth_date}
+          location={userData.country}
+          description={userData.description}
+        />
       </div>
     </div>
   );
-}
+};
+
+export default Profile;
