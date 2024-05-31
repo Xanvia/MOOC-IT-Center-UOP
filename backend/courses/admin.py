d6faf49321bc7fed5bfa60e8917c621132b73a4c
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-31 23:27:26 +0530

    add another models

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Title
+from .models import OfferedBy
+from .models import CourseCreator
+from .models import Duration
 
-admin.site.register(Title)
-# Register your models here.
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ("label", "description")
+
+class OfferdByAdmin(admin.ModelAdmin):
+    list_display = ("label", "institution")  
+
+class CourseCreatorAdmin(admin.ModelAdmin):
+    list_display = ("label", )  
+
+class DurationAdmin(admin.ModelAdmin):
+    list_display = ("label", "start_date", "end_date" )        
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(OfferedBy, OfferdByAdmin)
+admin.site.register(CourseCreator, CourseCreatorAdmin)
+admin.site.register(Duration, DurationAdmin)
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -4,3 +4,31 @@ from django.db import models
 class Title(models.Model):
     label = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.label
+
+
+class OfferedBy(models.Model):
+    label = models.CharField(max_length=150)
+    institution = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.label
+    
+
+class CourseCreator(models.Model):
+    label = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.label
+    
+
+class Duration(models.Model):
+    label = models.CharField(max_length=100)
+    start_date = models.CharField(max_length=7)
+    end_date = models.CharField(max_length=7,blank=True, null=True)
+
+    def __str__(self):
+        return self.label
+
