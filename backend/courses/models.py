dc4d5e7ba569f161e5ab6678be63dfa59e5d6500
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-05-31 22:30:10 +0530

    add titles model

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Title(models.Model):
+    label = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
