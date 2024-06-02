a774806a3b77277345b01ab04afb8443cccebff7
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-06-02 22:51:02 +0530

    feat: ceate course button and cards

--- frontend/src/app/(public)/courses/page.tsx
+++ frontend/src/app/(public)/courses/page.tsx
@@ -1,15 +1,44 @@
+"use client";
+import SecondaryButton from "@/components/Buttons/SecondaryButton";
+import CourseCard from "@/components/Course/CourseCard/CourseCard";
 import React from "react";
+
+const course2 = "/images/course2.png";
+const course3 = "/images/course3.png";
+const course5 = "/images/course5.png";
 
 export default function Courses() {
   return (
     <>
-      <div>
+      <div className="container mx-auto px-4 mt-20">
+        <div className="mx-36 flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold">My Courses</h1>
+          <SecondaryButton onClick={() => {}} text="Create Course" />
+        </div>
 
+        <div className="grid grid-cols-1 py-10 ml-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 justify-center items-center mx-10 sm:mx-36 lg:mx-36 gap-4  lg:gap-4 2xl:gap-10">
+          <CourseCard
+            title="Digital Marketing"
+            description="Material on beginner marketing strategies and concepts"
+            image={course2}
+          />
 
-        <h1>My learning</h1>
-       {/* Create course button */}
-
-       {/* CourseCards */}
+          <CourseCard
+            title="Data Analysis"
+            description="Description 3"
+            image={course3}
+          />
+          <CourseCard
+            title="Digital Marketing"
+            description="Material on beginner marketing strategies and concepts"
+            image={course5}
+          />
+          <CourseCard
+            title="Digital Marketing"
+            description="Material on beginner marketing strategies and concepts"
+            image={course2}
+          />
+        </div>
       </div>
     </>
   );
