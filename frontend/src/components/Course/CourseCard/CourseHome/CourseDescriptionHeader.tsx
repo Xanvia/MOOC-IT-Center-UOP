edd8a3c95ae381ff35aee9626dbc7ba9b7947bdd
KulinduThenuka <kulinduthenuka@gmail.com>
2024-06-03 01:52:22 +0530

    feat:course discription set tabs

--- frontend/src/components/Course/CourseCard/CourseHome/CourseDescriptionHeader.tsx
+++ frontend/src/components/Course/CourseCard/CourseHome/CourseDescriptionHeader.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CourseHeader: React.FC = () => {
-  return <></>;
+  return <>
+
+
+      <div className="bg-white shadow-md">
+        <div className="container mx-auto">
+          <nav className="flex space-x-8 py-4">
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="description">Description</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="instructor">Instructor</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="syllabus">Syllabus</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="prerequisite">Prerequisite</a>
+          </nav>
+        </div>
+      </div>
+  </>;
 };
 
 export default CourseHeader;
