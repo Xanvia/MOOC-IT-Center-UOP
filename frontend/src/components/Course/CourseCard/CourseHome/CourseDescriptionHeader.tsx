5745e8a8585f055a8daad97bf43aa20aa66a5b1d
KulinduThenuka <kulinduthenuka@gmail.com>
2024-06-03 02:59:10 +0530

    description content without image

--- frontend/src/components/Course/CourseCard/CourseHome/CourseDescriptionHeader.tsx
+++ frontend/src/components/Course/CourseCard/CourseHome/CourseDescriptionHeader.tsx
@@ -13,6 +13,36 @@ const CourseHeader: React.FC = () => {
             <a href="#" className="hover:text-blue-500 tab-link" data-tab="prerequisite">Prerequisite</a>
           </nav>
         </div>
+
+        <div className="container mx-auto my-8 p-4 bg-white shadow-md bg-sky-100">
+          {/* Description Content */}
+          <div>
+            <div id="description" className="tab-content active">
+              <h2 className="text-2xl font-bold mb-4">About the Course</h2>
+              <p className="mb-4">
+                This course provides an introduction to web development using HTML, CSS, and JavaScript. You learn how to create responsive and interactive web pages, and you'll gain a solid foundation in web programming.
+              </p>
+            </div>
+
+            <div>
+              {/* Add image */}
+            </div>
+
+            <div>
+              <ul className="list-disc">
+                <h1 className="font-bold">Specification and Software</h1>
+                <li className="ml-10">Pc/laptop</li>
+                <li className="ml-10">Browser</li>
+                <li className="ml-10">Pc/laptop</li>
+                <li className="ml-10">Browser</li>
+                
+              </ul>
+            
+            </div>
+
+          </div>
+        </div>
+
       </div>
   </>;
 };
