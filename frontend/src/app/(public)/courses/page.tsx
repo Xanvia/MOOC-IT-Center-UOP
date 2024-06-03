6ccf1bf79a540e56ad3e18f1eea038ea141a723d
AselDamsika <162301283+AselDamiska@users.noreply.github.com>
2024-06-03 22:25:28 +0530

    57 frontend create a course model (#96)
    * add CreateCourseModal to course page
    
    * feat:change textx sizes
    
    * add a dropdownto categories
    
    * fix: category as usual
    
    * feat: set standard colours
    
    * set formik to input label
    
    * feat: set paddings
    
    * feat: change model compoentn
    
    ---------
    
    Co-authored-by: Lakshitha Eranga <sanjueranga16@gmail.com>

--- frontend/src/app/(public)/courses/page.tsx
+++ frontend/src/app/(public)/courses/page.tsx
@@ -1,28 +1,34 @@
 "use client";
 import SecondaryButton from "@/components/Buttons/SecondaryButton";
 import CourseCard from "@/components/Course/CourseCard/CourseCard";
-import React from "react";
+import React, { useState } from "react";
+import CreateCourseModal from "@/components/CreateCourseModal/CreateCourseModal";
 
 const course2 = "/images/course2.png";
 const course3 = "/images/course3.png";
 const course5 = "/images/course5.png";
 
 export default function Courses() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
       <div className="container mx-auto px-4 mt-20">
         <div className="mx-36 flex justify-between items-center mb-8">
           <h1 className="text-3xl font-bold">My Courses</h1>
-          <SecondaryButton onClick={() => {}} text="Create Course" />
+          <CreateCourseModal />
         </div>
 
-        <div className="grid grid-cols-1 py-10 ml-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 justify-center items-center mx-10 sm:mx-36 lg:mx-36 gap-4  lg:gap-4 2xl:gap-10">
+        <div className="grid grid-cols-1 py-10 ml-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 justify-center items-center mx-10 sm:mx-36 lg:mx-36 gap-4 lg:gap-4 2xl:gap-10">
           <CourseCard
             title="Digital Marketing"
             description="Material on beginner marketing strategies and concepts"
             image={course2}
           />
-
           <CourseCard
             title="Data Analysis"
             description="Description 3"
