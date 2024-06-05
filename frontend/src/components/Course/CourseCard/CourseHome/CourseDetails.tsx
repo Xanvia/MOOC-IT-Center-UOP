1f13eed19c6b8f3189bb462d9fb7f2e08233b6e7
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-06-05 22:05:28 +0530

    add tabs for course details

--- frontend/src/components/Course/CourseCard/CourseHome/CourseDetails.tsx
+++ frontend/src/components/Course/CourseCard/CourseHome/CourseDetails.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const CourseDetails: React.FC = () => {
-  return <></>;
+  return (
+    <>  
+    <div className="bg-white mt-24 shadow-md">
+        <div className="container mx-auto">
+          <nav className="flex space-x-8 py-4">
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="description">Description</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="instructor">Instructor</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="syllabus">Syllabus</a>
+            <a href="#" className="hover:text-blue-500 tab-link" data-tab="prerequisite">Prerequisite</a>
+          </nav>
+        </div>
+    </div>
+    <h1 className="font-bold">
+      Hello
+    </h1>
+    </>
+  ); 
 };
 
 export default CourseDetails;
