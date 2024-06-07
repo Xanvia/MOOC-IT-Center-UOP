346ba60c7ffb19bd2fb67d4cc16faef476b37e0d
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-06-07 10:20:41 +0530

    add Accordian component

--- frontend/src/components/Course/CourseCard/CourseHome/CourseContent.tsx
+++ frontend/src/components/Course/CourseCard/CourseHome/CourseContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import CourseContentAccordion from "../CourseHome/CourseContentAccordion";
 
 const CourseContent: React.FC = () => {
   return <>
   <h1 className="text-3xl font-bold text-primary">Course Content</h1>
+  <CourseContentAccordion/>
   </>;
 };
 
