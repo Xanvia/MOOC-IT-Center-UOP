0e480558c96eaaf886e1c0f30c5058a43123a2f4
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-06-16 21:05:33 +0530

    course content set hover and colours

--- frontend/src/components/Course/CourseHome/CourseContent.tsx
+++ frontend/src/components/Course/CourseHome/CourseContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+
 interface AccordionItem {
   title: string;
   content: string;
@@ -31,61 +32,49 @@ const accordionData: AccordionItem[] = [
 const CourseContent: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(0);
 
+
   const handleToggle = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
   return (
-    <>
-      <div className="w-full">
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-2 xl:mx-12 my-24 md:mx-28 mx-10  lg:mx-40">
+    <div className="w-full">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-2 xl:mx-12 my-24 md:mx-28 mx-10 lg:mx-40">
+        <div>
+          <h1 className="text-3xl text-center font-bold text-primary">
+            Course Content
+          </h1>
+        </div>
+        <div className="xl:mr-56 pt-10 xl-pt-0">
           <div>
-            <h1 className="text-3xl text-center font-bold text-primary">
-              Course Content
-            </h1>
-          </div>
-          <div className="xl:mr-56 pt-10 xl-pt-0">
-            <div className="">
-              <div className=" ">
-                {accordionData.map((item, index) => (
-                  <div key={index}>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        cursor: "pointer",
-                        marginBottom: "32px",
-                        borderBottom: "1px solid black",
-                        paddingBottom: "8px",
-                      }}
-                      onClick={() => handleToggle(index)}
-                    >
-                      <span
-                        style={{
-                          color: activeIndex === index ? "blue" : "black",
-                        }}
-                      >
-                        {index + 1 < 10 ? `0${index + 1}.` : index + 1}{" "}
-                        {item.title}.
-                      </span>
+            {accordionData.map((item, index) => (
+              <div key={index}>
+                <div
+                  className="flex justify-between text-primary cursor-pointer mb-8 pb-2 border-b border-gray-800 transition-all duration-300 ease-in-out transform hover:scale-105"
+                  onClick={() => handleToggle(index)}
+                >
+                  <span className={`${
+                      activeIndex === index
+                        ? "font-bold"
+                        : "font"
+                    } transition-all duration-100`}
+                  >
+                    {index + 1 < 10 ? `0${index + 1}.` : index + 1} {item.title}.
+                  </span>
 
-                      <span>{activeIndex === index ? "x" : "+"}</span>
-                    </div>
-                    {activeIndex === index && (
-                      <div
-                        style={{ paddingLeft: "32px", marginBottom: "44px" }}
-                      >
-                        {item.content}
-                      </div>
-                    )}
+                  <span>{activeIndex === index ? "x" : "+"}</span>
+                </div>
+                {activeIndex === index && (
+                  <div style={{ paddingLeft: "32px", marginBottom: "44px" }}>
+                    {item.content}
                   </div>
-                ))}
+                )}
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
