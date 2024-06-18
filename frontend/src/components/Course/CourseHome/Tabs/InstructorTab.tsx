eeb9e8957c92b3e170b6ef5367c73f309f6b539a
SandakelumSameera <sandakelumsameera98@gmail.com>
2024-06-18 06:38:14 +0530

    get course details

--- frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
+++ frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
@@ -23,26 +23,32 @@ const instructors: Instructor[] = [
 
 const InstructorTab: React.FC = () => {
   return (
-    <div className="lg:mx-32">
-      <div className="py-14 px-3 sm:px-20 xl:mx-28  text-left bg-primary_light">
-    <div className="flex flex-col gap-4 bg-white rounded-lg">
-      {instructors.map((instructor, index) => (
-        <div key={index} className="p-4  rounded shadow flex  md:flex-row md:justify-between">
-          <div>
-            <h2 className="text-xl font-bold">{instructor.name}</h2>
-            <p><strong>Country:</strong> {instructor.country}</p>
-            <p><strong>University:</strong> {instructor.university}</p>
-          </div>
-          <div>
-            <p><strong>Duration:</strong> {instructor.duration}</p>
-            <p><strong>Subject Area:</strong> {instructor.subjectArea}</p>
-          </div>
-          <p>{instructor.description}</p>
-        </div>
-      ))}
-    </div>
-  </div>
-</div>
+
+     <div className="lg:mx-32">
+       <div className="py-14 px-3 sm:px-20 xl:mx-28  text-left bg-primary_light">
+     <div className="flex flex-col gap-4 bg-white rounded-lg">
+       {instructors.map((instructor, index) => (
+         <div key={index} className="p-4  rounded shadow md:flex-row md:justify-between">
+            <div className="flex space-x-10">
+                
+                <div>
+                  <h2 className="text-xl font-bold text-gray-900">{instructor.name}</h2>
+                  <p><strong>Country:</strong> {instructor.country}</p>
+                  <p><strong>University:</strong> {instructor.university}</p>
+                  <p><strong>Duration:</strong> {instructor.duration}</p>
+                  <p><strong>Subject Area:</strong> {instructor.subjectArea}</p>
+                </div> 
+              </div>
+              <div className="border-t border-gray-200">
+                <div className="px-6 py-4">
+                  <p>{instructor.description}</p>
+                </div>
+              </div>
+          </div>              
+       ))}
+     </div>
+   </div>
+ </div>
   );
 };
 
