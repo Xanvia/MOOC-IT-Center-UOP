426dc9078491c98d490155bb0b561be04c85ba10
AselDamsika <162301283+AselDamiska@users.noreply.github.com>
2024-06-18 11:25:39 +0530

    134 frontend  course room side bar (#135)
    * feat: create new room page
    
    * feat: created new sidebar component
    
    * feat: set a according into sidebar
    
    * feat:set select topics in sidebar
    
    * change topic names
    
    * feat: add some components into contents
    
    * feat: set allignments in sidebar
    
    * feat: add colours to page and sidebar
    
    * WIP
    
    * feat: add breadcrumb to courseroom
    
    * remove breadcrumb from header and put it to course id page
    
    * feat: set transition hover and border bottom to sidebar weeks
    
    * feat: add a border bottom to sidebar topics
    
    * course content set hover and colours
    
    * feat: new side bar func
    
    * feat: expand multiple weeks at the sametime
    
    * feat: increased sidebar width
    
    * add new icons from tailwindtoolbox
    
    * feat: set allingments titles
    
    * feat: expand only one week in that time
    
    ---------
    
    Co-authored-by: Lakshitha Eranga <sanjueranga16@gmail.com>

--- frontend/src/app/(public)/courses/[id]/page.tsx
+++ frontend/src/app/(public)/courses/[id]/page.tsx
@@ -1,14 +1,25 @@
-import CourseContent from "@/components/Course/CourseHome/CourseContent";
 import CourseHeader from "@/components/Course/CourseHome/CourseDescriptionHeader";
-import CourseOutcomes from "@/components/Course/CourseHome/CourseOutcomes";
 import CourseHStat from "@/components/Course/CourseHome/CourseHStat";
 import ReccomendedCourses from "@/components/Course/CourseCard/ReccomendedCourses";
 import CourseDetailsTabs from "@/components/Course/CourseHome/Tabs/CourseDetailsTabs";
+import Breadcrumb from "@/components/Course/CourseHome/Breadcrumb";
 import React from "react";
+
+interface BreadcrumbItem {
+  breadcrumb: string;
+  href?: string; // Make href optional
+}
+
+const breadcrumbs: BreadcrumbItem[] = [
+  { breadcrumb: "Home", href: "/" },
+  { breadcrumb: "> Courses", href: "/courses" },
+  { breadcrumb: "> Basic Web Programming", href: "/courses/1" },
+];
 
 export default function CoursesHome() {
   return (
     <>
+      <Breadcrumb breadcrumbs={breadcrumbs} />
       <CourseHeader />
       <CourseHStat />
 
