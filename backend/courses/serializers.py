85e10c82abfb8c72ba7535dfa2d238432e401199
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-06-30 17:08:04 +0530

    create week object

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,27 +1,12 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Course
-from userprofiles.models import Institution
-from userprofiles.serializers import InterestSerializer
-
-
-class CourseTeacherSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ["email", "first_name", "last_name"]
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation["full_name"] = f"{instance.first_name} {instance.last_name}"
-        representation["headline"] = instance.userprofile.headline
-        representation.pop("first_name")
-        representation.pop("last_name")
-        return representation
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import AccessToken
+from .models import Course, Week
+from userprofiles.models import Interest
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    institution = serializers.CharField()
 
     class Meta:
         model = Course
@@ -29,23 +14,16 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         request = self.context.get("request")
-
-        if request and request.method == "POST" or request.method == "PUT":
-            institution_name = data.pop("institution")
-            institution, _ = Institution.objects.get_or_create(label=institution_name)
-            data["institution"] = institution
-
         if request and request.method == "PATCH":
-            allowed_fields = {"outcomes", "specifications","description"}
+            allowed_fields = {"outcomes", "specifications"}
             # Filter the data to only include allowed fields
             data = {key: value for key, value in data.items() if key in allowed_fields}
         return data
+    
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation["course_creator"] = CourseTeacherSerializer(
-            instance.course_creator
-        ).data
-        representation["category"] = InterestSerializer(instance.category).data
-        representation["institution"] = instance.institution.label
-        return representation
+class WeekSerializer(serializers.ModelSerializer):
+    course_id = serializers.IntegerField
+
+    class Meta:
+        model = Week
+        fields = ["course_id","name"]
