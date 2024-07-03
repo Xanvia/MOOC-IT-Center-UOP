714f8051944f3f198bce9136c1653a8de4b0ff6d
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-07-03 22:14:31 +0530

    feat: set breadcrumb to course room layout

--- frontend/src/app/(public)/courses/[id]/week-[number]/layout.tsx
+++ frontend/src/app/(public)/courses/[id]/week-[number]/layout.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 import Sidebar from "@/components/Course/Sidebar/Sidebar";
 import { SelectedTopicProvider } from "@/contexts/SidebarContext";
+import Breadcrumb from "@/components/Course/CourseHome/Breadcrumb";
 
 interface InnerLayoutProps {
   children: React.ReactNode;
 }
 
+interface BreadcrumbItem {
+  breadcrumb: string;
+  href?: string;
+}
+
+const breadcrumbs: BreadcrumbItem[] = [
+  { breadcrumb: "Home", href: "/" },
+  { breadcrumb: "> Courses", href: "/courses" },
+  { breadcrumb: "> Basic Web Programming", href: "/courses/1" },
+  { breadcrumb: "> Course Room", href: "/courses/1/room" },
+];
+
 const InnerLayout: React.FC<InnerLayoutProps> = ({ children }) => {
   return (
-    <SelectedTopicProvider>
-      <div className="flex" style={{ height: '100%' }}>
-        <div style={{ flexShrink: 0 }}>
-          <Sidebar />
+    <>
+    <Breadcrumb breadcrumbs={breadcrumbs} />
+      <SelectedTopicProvider>
+        <div className="flex" style={{ height: '100%' }}>
+          <div style={{ flexShrink: 0 }}>
+            <Sidebar />
+          </div>
+          <div style={{ flexGrow: 1 }}>
+            {children}
+          </div>
         </div>
-        <div style={{ flexGrow: 1 }}>
-          {children}
-        </div>
-      </div>
-    </SelectedTopicProvider>
+      </SelectedTopicProvider>
+    </>
   );
 };
 
--- frontend/src/app/(public)/courses/[id]/week-[number]/page.tsx
+++ frontend/src/app/(public)/courses/[id]/week-[number]/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Note from "@/components/Course/Note/Note";
+import Breadcrumb from "@/components/Course/CourseHome/Breadcrumb";
+
+
+
 
 const Page: React.FC = () => {
   return (
-    <div className="flex-grow p-4">
-      <Note/>
-    </div>
+    <>
+      
+      
+      <div className="flex-grow p-4">
+        <Note/>
+      </div>
+    </>
   );
 };
 
