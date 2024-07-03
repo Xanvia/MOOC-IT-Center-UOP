1dbc29992f5be52b5b46e6ecaa804a91b3fabb1e
Lakshitha Eranga <90528675+sanjueranga@users.noreply.github.com>
2024-07-03 11:20:38 +0530

    Dynamic loading (#163)
    * WIP
    
    * WIP
    
    * WIP

--- frontend/src/app/(public)/profile/page.tsx
+++ frontend/src/app/(public)/profile/page.tsx
@@ -1,12 +1,22 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import Profile from "@/components/Profile/Profile";
-import ExperienceModal from "@/components/Profile/Experience/WorkExperienceModal";
-import ExperienceCard from "@/components/Profile/Experience/WorkExperienceCard";
-import EducationCard from "@/components/Profile/Education/EducatonCard";
-import EducationModal from "@/components/Profile/Education/EducationModal";
+import React, { useState, useEffect, Suspense } from "react";
 import { Work, Education, ProfileData } from "@/components/Profile/types";
 import { fetchProfileData } from "@/services/user.service";
+
+const Profile = React.lazy(() => import("@/components/Profile/Profile"));
+
+const EducationModal = React.lazy(
+  () => import("@/components/Profile/Education/EducationModal")
+);
+const EducationCard = React.lazy(
+  () => import("@/components/Profile/Education/EducatonCard")
+);
+const ExperienceCard = React.lazy(
+  () => import("@/components/Profile/Experience/WorkExperienceCard")
+);
+const ExperienceModal = React.lazy(
+  () => import("@/components/Profile/Experience/WorkExperienceModal")
+);
 
 export default function ProfilePage() {
   const [work, setWork] = useState<Work[]>([]);
@@ -38,43 +48,51 @@ export default function ProfilePage() {
   return (
     <>
       <div className="flex flex-col lg:flex-row w-full py-20">
-        <Profile reloadData={reloadData} profileData={profileData} />
+        <Suspense>
+          <Profile reloadData={reloadData} profileData={profileData} />
+        </Suspense>
         <div className="relative  lg:w-full h-11/12  md:rounded-r-lg lg:basis-1/2 2xl:px-12 basis-1/3 mx-6 sm:ml-32 pt-32 lg:pt-0 lg:m-0">
           <div className="min-h-[300px]">
-            <EducationModal
-              CardTitle="Add Your Education Details"
-              reloadData={reloadData}
-            />
-            {education && education.length > 0 ? (
-              education.map((eduItem) => (
-                <EducationCard
-                  key={eduItem.id}
-                  eduData={eduItem}
-                  reload={reloadData}
-                />
-              ))
-            ) : (
-              <EducationCard eduData={education[0]} reload={reloadData} />
-            )}
+            <Suspense>
+              <EducationModal
+                CardTitle="Add Your Education Details"
+                reloadData={reloadData}
+              />
+            </Suspense>
+            <Suspense>
+              {education && education.length > 0 ? (
+                education.map((eduItem) => (
+                  <EducationCard
+                    key={eduItem.id}
+                    eduData={eduItem}
+                    reload={reloadData}
+                  />
+                ))
+              ) : (
+                <EducationCard eduData={education[0]} reload={reloadData} />
+              )}
+            </Suspense>
           </div>
           <div className="py-3">
-            <ExperienceModal
-              CardTitle="Add Your Work Experience"
-              reloadData={reloadData}
-            />
-            {work && work.length > 0 ? (
-              work.map((workItem) => (
-                <ExperienceCard
-                  key={workItem.id}
-                  workData={workItem}
-                  reload={reloadData}
-                />
-              ))
-            ) : (
-              <div className="py-10 text-xl">
-                <p>Add your work experience here</p>
-              </div>
-            )}
+            <Suspense>
+              <ExperienceModal
+                CardTitle="Add Your Work Experience"
+                reloadData={reloadData}
+              />
+              {work && work.length > 0 ? (
+                work.map((workItem) => (
+                  <ExperienceCard
+                    key={workItem.id}
+                    workData={workItem}
+                    reload={reloadData}
+                  />
+                ))
+              ) : (
+                <div className="py-10 text-xl">
+                  <p>Add your work experience here</p>
+                </div>
+              )}
+            </Suspense>
           </div>
         </div>
       </div>
