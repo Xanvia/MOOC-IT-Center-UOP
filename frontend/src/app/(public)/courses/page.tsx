99e895b3605fc36a1060a1d71ead3b17405a9374
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-07-03 11:17:56 +0530

    WIP

--- frontend/src/app/(public)/courses/page.tsx
+++ frontend/src/app/(public)/courses/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from "react";
+import React, { useState, Suspense } from "react";
 import CourseCard from "@/components/Course/CourseCard/CourseCard";
 import Breadcrumb from "@/components/Course/CourseHome/Breadcrumb";
-import CreateCourseModal from "@/components/Course/CreateCourseModal/CreateCourseModal";
-
-import React from "react";
+const CreateCourseModal = React.lazy(
+  () => import("@/components/Course/CreateCourseModal/CreateCourseModal")
+);
 
 const course2 = "/images/course2.png";
 const course3 = "/images/course3.png";
@@ -34,7 +34,9 @@ export default function Courses() {
       <div className="container mx-auto px-4 mt-20">
         <div className="mx-36 flex justify-between items-center mb-8">
           <h1 className="text-3xl font-bold">My Courses</h1>
-          <CreateCourseModal />
+          <Suspense>
+            <CreateCourseModal />
+          </Suspense>
         </div>
 
         <div className="grid grid-cols-1 py-10 ml-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 justify-center items-center mx-10 sm:mx-36 lg:mx-36 gap-4 lg:gap-4 2xl:gap-10">
