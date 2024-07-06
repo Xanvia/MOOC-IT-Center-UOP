3e2778b659fa9fa1dbdb30b031ad7e6997c4c1c7
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-07-06 12:13:32 +0530

    feat: tiny

--- frontend/src/components/Course/Note/NoteEditor.tsx
+++ frontend/src/components/Course/Note/NoteEditor.tsx
@@ -1,29 +1,58 @@
-import React, { useState } from "react";
-import JoditReact from "jodit-react-ts";
-import "jodit/es5/jodit.min.css";
-
-interface NoteEditorProps {
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { TINY_API_KEY } from "@/utils/constants";
+interface TextEditorProps {
   initialData: string;
 }
 
-const NoteEditor = ({ initialData }: NoteEditorProps) => {
-  const [value, setValue] = useState(initialData);
+const TextEditor: React.FC<TextEditorProps> = ({initialData}) => {
+  const editorRef = useRef<any>(null);
 
-  const config = {
-    uploader: {
-      insertImageAsBase64URI: false,
-    },
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
   };
 
   return (
     <div>
-      <JoditReact
-        config={config}
-        onChange={(content) => setValue(content)}
-        defaultValue={value}
+      <Editor
+        apiKey={TINY_API_KEY}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue={initialData}
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
       />
     </div>
   );
 };
 
-export default NoteEditor;
+export default TextEditor;
