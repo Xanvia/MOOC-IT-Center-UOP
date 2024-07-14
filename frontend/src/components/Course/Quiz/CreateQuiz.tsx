d007785cfb6225ae38c0dfa27ff69937fcb477b7
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-07-14 22:51:13 +0530

    feat: create course background

---
+++ frontend/src/components/Course/Quiz/CreateQuiz.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from "react";
+
+const CreateQuiz: React.FC = () => {return (
+    <div className="quiz-container p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h1 className="text-3xl font-bold mb-4">Quiz</h1>
+      
+    </div>
+  );
+};
+
+export default CreateQuiz;
