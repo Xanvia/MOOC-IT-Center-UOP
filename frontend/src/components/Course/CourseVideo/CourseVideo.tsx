915dfefb98bc223b847859fc85c45b9e187ca6cc
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-07-19 20:45:55 +0530

    WIP

--- frontend/src/components/Course/CourseVideo/CourseVideo.tsx
+++ frontend/src/components/Course/CourseVideo/CourseVideo.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { HOST } from "@/utils/constants";
-import EditButtonPrimary from "@/components/Buttons/EditButtonPrimary";
 import { toast } from "sonner";
 
 interface CourseVideoProps {
@@ -10,7 +9,7 @@ interface CourseVideoProps {
 }
 
 const CourseVideo: React.FC<CourseVideoProps> = ({ videoURL, title }) => {
-  const [editView, setEditView] = useState(false);
+  const isEdit = true;
   const videoRef = useRef<HTMLVideoElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(1);
@@ -19,7 +18,9 @@ const CourseVideo: React.FC<CourseVideoProps> = ({ videoURL, title }) => {
   const [uploadedVideoURL, setUploadedVideoURL] = useState<string | null>(null);
   const [uploadStatus, setUploadStatus] = useState<string | null>(null);
 
-  const VideoSource = uploadedVideoURL ? uploadedVideoURL : `${HOST}${videoURL}`;
+  const VideoSource = uploadedVideoURL
+    ? uploadedVideoURL
+    : `${HOST}${videoURL}`;
 
   const handlePlayPause = () => {
     if (videoRef.current) {
@@ -75,142 +76,133 @@ const CourseVideo: React.FC<CourseVideoProps> = ({ videoURL, title }) => {
     if (!uploadedVideoFile) return;
 
     const formData = new FormData();
-    formData.append('video', uploadedVideoFile);
+    formData.append("video", uploadedVideoFile);
 
     try {
-      setUploadStatus('Uploading...');
+      setUploadStatus("Uploading...");
       const response = await fetch(`${HOST}/upload`, {
-        method: 'POST',
+        method: "POST",
         body: formData,
       });
 
       if (response.ok) {
         const data = await response.json();
-        setUploadStatus('Upload successful');
+        setUploadStatus("Upload successful");
         setUploadedVideoURL(data.videoURL); // Assuming the server returns the URL of the uploaded video
       } else {
-        setUploadStatus('Upload failed');
+        setUploadStatus("Upload failed");
       }
     } catch (error) {
-      console.error('Upload error:', error);
-      setUploadStatus('Upload failed');
+      console.error("Upload error:", error);
+      setUploadStatus("Upload failed");
     }
   };
 
   return (
     <div className="my-14 mx-12 p-3 border  bg-blue-100 border-gray-200 shadow-lg rounded-lg">
-      {!editView ? (
-        <div className="flex m-4">
-          <EditButtonPrimary
-            text="E D I T"
-            onClick={() => setEditView(true)}
-          />
-        </div>
-      ) : (
-        <>
-          <div className="my-14 mx-12 p-3 border bg-white border-gray-200 shadow-lg rounded-lg">
-      <h2 className="text-2xl font-semibold mx-20 mb-4">{title}</h2>
-      <div className="relative my-4 group">
-        <video
-          ref={videoRef}
-          className="w-full h-auto max-h-96 object-cover"
-          onEnded={handleVideoEnd}
-          controls={false}
-        >
-          <source src={VideoSource} type="video/mp4" />
-        </video>
-        <div className="absolute inset-0 flex justify-center items-center">
-          {isPlaying && !isEnded && (
-            <button
-              onClick={handlePlayPause}
-              className="hidden group-hover:block shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
+      <>
+        <div className="my-14 mx-12 p-3 border bg-white border-gray-200 shadow-lg rounded-lg">
+          <h2 className="text-2xl font-semibold mx-20 mb-4">{title}</h2>
+          <div className="relative my-4 group">
+            <video
+              ref={videoRef}
+              className="w-full h-auto max-h-96 object-cover"
+              onEnded={handleVideoEnd}
+              controls={false}
             >
-              <svg
-                className="w-16 h-16 text-yellow-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              <source src={VideoSource} type="video/mp4" />
+            </video>
+            <div className="absolute inset-0 flex justify-center items-center">
+              {isPlaying && !isEnded && (
+                <button
+                  onClick={handlePlayPause}
+                  className="hidden group-hover:block shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
+                >
+                  <svg
+                    className="w-16 h-16 text-yellow-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 011.414 1.414L11.414 11l4.293 4.293a1 1 0 01-1.414 1.414L10 12.414l-4.293 4.293a1 1 0 01-1.414-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+              {(!isPlaying || isEnded) && (
+                <button
+                  onClick={handlePlayPause}
+                  className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
+                >
+                  <svg
+                    className="w-16 h-16 text-yellow-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6.293 4.293a1 1 0 011.414 0L13 9.586a1 1 0 010 1.414L7.707 16.707a1 1 0 01-1.414-1.414L10.586 11 6.293 6.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+              {isEnded && (
+                <button
+                  onClick={handleReplay}
+                  className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
+                >
+                  <svg
+                    className="w-16 h-16 text-yellow-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6.293 4.293a1 1 0 011.414 0L13 9.586a1 1 0 010 1.414L7.707 16.707a1 1 0 01-1.414-1.414L10.586 11 6.293 6.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={handleVolumeChange}
+                className="mr-2"
+              />
+              <span>{Math.round(volume * 100)}%</span>
+            </div>
+            <div>
+              <input
+                type="file"
+                accept="video/*"
+                onChange={handleFileUpload}
+                className="hidden"
+                id="file-upload"
+              />
+              <label
+                htmlFor="file-upload"
+                className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out bg-blue-200 text-black text-sm font-semibold hover:bg-blue-400 px-4 py-2 rounded cursor-pointer"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 011.414 1.414L11.414 11l4.293 4.293a1 1 0 01-1.414 1.414L10 12.414l-4.293 4.293a1 1 0 01-1.414-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          )}
-          {(!isPlaying || isEnded) && (
-            <button
-              onClick={handlePlayPause}
-              className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
-            >
-              <svg
-                className="w-16 h-16 text-yellow-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M6.293 4.293a1 1 0 011.414 0L13 9.586a1 1 0 010 1.414L7.707 16.707a1 1 0 01-1.414-1.414L10.586 11 6.293 6.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          )}
-          {isEnded && (
-            <button
-              onClick={handleReplay}
-              className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out"
-            >
-              <svg
-                className="w-16 h-16 text-yellow-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M6.293 4.293a1 1 0 011.414 0L13 9.586a1 1 0 010 1.414L7.707 16.707a1 1 0 01-1.414-1.414L10.586 11 6.293 6.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          )}
+                Upload Video
+              </label>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
-            className="mr-2"
-          />
-          <span>{Math.round(volume * 100)}%</span>
-        </div>
-        <div>
-          <input
-            type="file"
-            accept="video/*"
-            onChange={handleFileUpload}
-            className="hidden"
-            id="file-upload"
-          />
-          <label
-            htmlFor="file-upload"
-            className="shadow-lg transform hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out bg-blue-200 text-black text-sm font-semibold hover:bg-blue-400 px-4 py-2 rounded cursor-pointer"
-          >
-            Upload Video
-          </label>
-        </div>
-      </div>
-    </div>
-        </>
-      )}
+      </>
     </div>
   );
 };
