f86df4fb24c64bae127c95acc3a9f2a473bb57b9
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-09-18 11:57:52 +0530

    WIP

--- backend/coursemanagement/serializers.py
+++ backend/coursemanagement/serializers.py
@@ -1,7 +1,3 @@
 from rest_framework import serializers
 from .models import Course
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'description']
