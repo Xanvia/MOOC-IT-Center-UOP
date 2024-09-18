a70a933b3b0e3c18f3cb18176485aea499b0a444
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-09-18 13:27:20 +0530

    feat: add teacher views

--- backend/coursemanagement/serializers.py
+++ backend/coursemanagement/serializers.py
@@ -1,3 +1,11 @@
 from rest_framework import serializers
-from .models import Course
+from .models import CourseTeachers
+
+
+class CourseTeachersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseTeachers
+        fields = "__all__"
+
+    
 
--- backend/coursemanagement/views.py
+++ backend/coursemanagement/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CourseTeacherSerializer
+from .models import CourseTeachers
+from .permissions import IsCourseCreator
 
-# Create your views here.
+
+class CourseTeacherViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseTeacherSerializer
+    queryset = CourseTeachers.objects.all()
+    permission_classes = [IsCourseCreator]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response.data = {
+            "message": "Teacher added to course",
+            "data": response.data,
+        }
+        return response
