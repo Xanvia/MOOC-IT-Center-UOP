71c35b9c551f4f41b4e16f62cd7f6c85e258f08a
KulinduThenuka <kulinduthenuka@gmail.com>
2024-09-30 00:41:00 +0530

    feat:add data stats component and dashboard setting

--- frontend/src/app/settings/[courseId]/page.tsx
+++ frontend/src/app/settings/[courseId]/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import DataStatsOne from "@/components/DataStats/DataStats";
 
 const CourseSettingsPage: React.FC = () => {
   return (
     <div>
       <h1>Course Settings</h1>
       <p>Manage your course settings here.</p>
+      
+      <DataStatsOne />
     </div>
   );
 };
