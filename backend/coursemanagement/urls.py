74f667a11e1642b0948325ceb606f480a33c74d1
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-10-14 15:17:53 +0530

    add the url path with EditPermissionAPIView

--- backend/coursemanagement/urls.py
+++ backend/coursemanagement/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import CourseTeacherViewSet
+from .views import CourseTeacherViewSet,EditPermissionAPIView
 
 
 urlpatterns = [
     path(
         "<int:course_id>/add-teacher/", CourseTeacherViewSet.as_view({"post": "create"},), name="course-teacher-list"
     ),
+    # path(
+    #     "<int:course_id>/add-teacher/", EditPermissionAPIView.as_view({"post": "create", "put": "update"},), name="course-teacher-list"
+    # ),
+    path(
+        "course-teacher/<int:pk>/edit-permissions/", EditPermissionAPIView.as_view(), name="edit-permissions"
+    ),
+
 ]
