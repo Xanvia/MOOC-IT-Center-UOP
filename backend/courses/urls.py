7808312ff25b1a90a97e9dedf6d01d5e6feadbd2
KulinduThenuka <kulinduthenuka@gmail.com>
2024-11-06 22:27:16 +0530

    view unpublished courses

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -40,6 +40,11 @@ urlpatterns = [
         CourseViewSet.as_view({"get": "list", "post": "create"}),
         name="course-list",
     ),
+     path(
+        "unpublished",
+        CourseViewSet.as_view({"get": "unpublished"}),
+        name="course-list",
+    ),
     path(
         "my-courses/", CourseViewSet.as_view({"get": "my_courses"}), name="my-courses"
     ),
