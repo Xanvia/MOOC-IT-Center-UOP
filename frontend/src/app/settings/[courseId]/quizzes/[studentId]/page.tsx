61fbf6e418f34119c54eb45e9fbcfbba30814a02
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-11-09 22:04:33 +0530

    feat: create quizzes/[studentID page]

---
+++ frontend/src/app/settings/[courseId]/quizzes/[studentId]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+
+const QuizzesManagementPage = () => {
+  return (
+    <div className="flex h-screen bg-gray-50">
+        <Head>
+            <title>OpenEd - Quizzes</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        
+
+        
+    </div>
+    
+  );
+};
+
+export default QuizzesManagementPage;
