67831d2f7ec9a42f9dbb511418153ddbdeb6c1c0
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-11-20 13:24:43 +0530

    fix:static roots

--- backend/mooc/settings.py
+++ backend/mooc/settings.py
@@ -20,6 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
