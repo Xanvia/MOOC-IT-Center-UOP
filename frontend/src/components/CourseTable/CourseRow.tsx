d2eff4ab2ec3cede476ad2ff48d0c5b043d9ce1c
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-11-24 13:24:37 +0530

    fix

--- frontend/src/components/CourseTable/CourseRow.tsx
+++ frontend/src/components/CourseTable/CourseRow.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Eye } from "lucide-react";
-import Courses from "@/app/(public)/courses/page";
+
 
 interface Course {
   id: number;
