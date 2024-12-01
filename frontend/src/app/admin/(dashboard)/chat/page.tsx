ad9473932b6db625c9b64fe3d2ec7f0ad9bd5afa
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-12-02 02:54:36 +0530

    feat: chat component

--- frontend/src/app/admin/(dashboard)/chat/page.tsx
+++ frontend/src/app/admin/(dashboard)/chat/page.tsx
@@ -1,32 +1,20 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Chat from "@/components/Chat/Chat";
 
-import {
-  getAllPublishedCourses,
-  getAllUnpublishedCourses,
-  publishCourse,
-} from "@/services/admin.service";
 
-interface Course {
-  id: number;
-  course_creator: string;
-  name: string;
-  difficulty: "Beginner" | "Intermediate" | "Advanced";
-}
-
-const CoursePage: React.FC = () => {
+const ChatPage: React.FC = () => {
   
 
   return (
     <div className="flex h-screen bg-gray-50">
       <main className="flex-1 flex flex-col overflow-hidden p-6 bg-white rounded-lg shadow-md mx-6 my-6">
-        <h2 className="text-2xl font-semibold mb-6 border-b pb-4">
-          Chat
-        </h2>
+        
+        <Chat />
 
       </main>
     </div>
   );
 };
 
-export default CoursePage;
+export default ChatPage;
