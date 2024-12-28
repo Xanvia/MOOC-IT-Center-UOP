24e1fdaa34f447e28897e307a6b5a2f3a15aeebc
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-12-28 21:20:59 +0530

    feat: add email and center

--- frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
+++ frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
@@ -14,8 +14,8 @@ const Example: React.FC<Props> = ({ instructors }) => {
         <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
           <div className="lg:mx-32"></div>
-          <figure className="mt-10">
-            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9"></blockquote>
+          <figure className="mt-10 text-center">
+            <blockquote className="text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9"></blockquote>
             <figcaption className="mt-10">
               {instructors.map((instructor, index) => (
                 <div
@@ -25,14 +25,23 @@ const Example: React.FC<Props> = ({ instructors }) => {
                   <Image
                     alt=""
                     src={instructor.profile_picture}
-                    className="mx-auto h-10 w-10 rounded-full"
+                    className="h-10 w-10 rounded-full"
                     width={40}
                     height={40}
                   />
                   <div className="font-semibold text-gray-900">
-                    {" "}
                     {instructor.full_name}
                   </div>
+                  <svg
+                    width={3}
+                    height={3}
+                    viewBox="0 0 2 2"
+                    aria-hidden="true"
+                    className="fill-gray-900"
+                  >
+                    <circle r={1} cx={1} cy={1} />
+                  </svg>
+                  <div className="text-gray-600">{instructor.email}</div>
                   <svg
                     width={3}
                     height={3}
