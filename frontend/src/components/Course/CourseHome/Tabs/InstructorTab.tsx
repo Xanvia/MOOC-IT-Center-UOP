070aacd511deed4dc8476185ccef22693da4fd02
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-12-28 21:35:25 +0530

    feat: allign

--- frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
+++ frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
@@ -9,53 +9,36 @@ interface Props {
 const Example: React.FC<Props> = ({ instructors }) => {
   return (
     <div>
-      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <section className="relative isolate overflow-hidden bg-white px-6 py-6 sm:py-32 lg:px-8">
         <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
         <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
-          <div className="lg:mx-32"></div>
-          <figure className="mt-10 text-center">
+        <div className="lg:mx-32"></div>
+          <figure className="mt-2 text-center">
             <blockquote className="text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9"></blockquote>
-            <figcaption className="mt-10">
+            <figcaption className="mt-2 flex flex-wrap justify-center gap-20">
               {instructors.map((instructor, index) => (
                 <div
                   key={index}
-                  className="mt-4 flex items-center justify-center space-x-3 text-base"
+                  className="flex flex-col items-center space-y-1 text-base"
                 >
                   <Image
                     alt=""
                     src={instructor.profile_picture}
-                    className="h-10 w-10 rounded-full"
-                    width={40}
-                    height={40}
+                    className="h-12 w-12 rounded-full"
+                    width={48}
+                    height={48}
                   />
                   <div className="font-semibold text-gray-900">
                     {instructor.full_name}
                   </div>
-                  <svg
-                    width={3}
-                    height={3}
-                    viewBox="0 0 2 2"
-                    aria-hidden="true"
-                    className="fill-gray-900"
-                  >
-                    <circle r={1} cx={1} cy={1} />
-                  </svg>
                   <div className="text-gray-600">{instructor.email}</div>
-                  <svg
-                    width={3}
-                    height={3}
-                    viewBox="0 0 2 2"
-                    aria-hidden="true"
-                    className="fill-gray-900"
-                  >
-                    <circle r={1} cx={1} cy={1} />
-                  </svg>
                   <div className="text-gray-600">{instructor.headline}</div>
                 </div>
               ))}
             </figcaption>
           </figure>
+
         </div>
       </section>
     </div>
