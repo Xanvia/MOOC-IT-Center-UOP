1b9131b4f405421783fdd83719985c8c385daaf5
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-12-29 10:10:55 +0530

    feat: py

--- frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
+++ frontend/src/components/Course/CourseHome/Tabs/InstructorTab.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Example: React.FC<Props> = ({ instructors }) => {
   return (
     <div>
-      <section className="relative isolate overflow-hidden bg-white px-6 py-6 sm:py-32 lg:px-8">
+      <section className="relative isolate overflow-hidden bg-white px-6 py-6 sm:py-16 lg:px-8">
         <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
         <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
@@ -32,8 +32,8 @@ const Example: React.FC<Props> = ({ instructors }) => {
                   <div className="font-semibold text-gray-900">
                     {instructor.full_name}
                   </div>
-                  <div className="text-gray-600">{instructor.email}</div>
                   <div className="text-gray-600">{instructor.headline}</div>
+                  <div className="text-gray-600">{instructor.email}</div>
                 </div>
               ))}
             </figcaption>
