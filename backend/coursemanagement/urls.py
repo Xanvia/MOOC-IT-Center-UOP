1c5ecda451a2aac1af17ad91d45f9bb03ba91a93
KulinduThenuka <kulinduthenuka@gmail.com>
2025-01-20 01:32:45 +0530

    set urls

--- backend/coursemanagement/urls.py
+++ backend/coursemanagement/urls.py
@@ -110,5 +110,5 @@ urlpatterns = [
     ),
     path("admin-messages/<int:course_id>", AdminMessagesViewSet.as_view({"get": "list","post":"create"}), name="admin-messages",),
     path("payments/<int:course_id>", PaymentsListAPIView.as_view(), name="payments-list"),
-    path('api/course-stats/', CourseStatView.as_view(), name='course-stats'),
+    path("settings/<int:pk>", CourseStatView.as_view(), name='course-stats'),
 ]
