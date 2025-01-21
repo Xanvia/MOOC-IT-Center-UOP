eb4f01ea4a267765ab4e227fc8ea69d33ff108dc
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2025-01-21 23:16:08 +0530

    feat: remove searchbar in admin dashboard

--- frontend/src/components/layout/HeaderWithoutChat.tsx
+++ frontend/src/components/layout/HeaderWithoutChat.tsx
@@ -41,7 +41,7 @@ const HeaderWchat = ({ toggleSidebar }: HeaderWchatProps) => {
         </div>
 
         <div className="flex items-center space-x-4">
-          <div
+          {/* <div
             className={`relative ${
               isSearchOpen ? "w-64" : "w-8"
             } transition-all duration-300`}
@@ -59,7 +59,7 @@ const HeaderWchat = ({ toggleSidebar }: HeaderWchatProps) => {
             >
               <Search size={20} />
             </button>
-          </div>
+          </div> */}
 
           <ProfileButton
             isProfileMenuOpen={isProfileMenuOpen}
